2a49860804292d87671f239ad5ca38413afb5f50
geon8692 <geon4735@gmail.com>
2024-07-20 22:57:30 +0900

    Test: 배포 테스트 코드 작성

--- src/main/java/sixgarlic/potenday/main/MainController.java
+++ src/main/java/sixgarlic/potenday/main/MainController.java
@@ -1,2 +1,15 @@
-package sixgarlic.potenday.main;public class MainController {
+package sixgarlic.potenday.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    @GetMapping("/main")
+    public String testApi() {
+        return "main";
+    }
 }
