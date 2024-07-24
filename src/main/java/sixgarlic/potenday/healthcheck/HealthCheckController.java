8b2a1933d89f4dcc9dcb4ff1060e4acc001bbbbc
geon8692 <geon4735@gmail.com>
2024-07-24 23:37:59 +0900

    Chore: Health Check

---
+++ src/main/java/sixgarlic/potenday/healthcheck/HealthCheckController.java
@@ -0,0 +1,14 @@
+package sixgarlic.potenday.healthcheck;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity healthCheck() {
+        return ResponseEntity.ok().body("hello");
+    }
+}
