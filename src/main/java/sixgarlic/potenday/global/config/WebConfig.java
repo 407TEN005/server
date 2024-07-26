6b779ea77dff1c079e7b7f33f8d38f2d9bfa8560
geon8692 <geon4735@gmail.com>
2024-07-27 01:55:06 +0900

    Fix: 배포환경 URL 수정

--- src/main/java/sixgarlic/potenday/global/config/WebConfig.java
+++ src/main/java/sixgarlic/potenday/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://potenday-sixgarlic.site")
+                .allowedOrigins("https://tenten.potenday-sixgarlic.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
