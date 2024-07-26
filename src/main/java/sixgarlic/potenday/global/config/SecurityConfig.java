9cbced93cced24a61f67bfa207fca58c515ba3c7
geon8692 <geon4735@gmail.com>
2024-07-26 19:24:03 +0900

    Fix: 시큐리티 컴파일 오류 수정

--- src/main/java/sixgarlic/potenday/global/config/SecurityConfig.java
+++ src/main/java/sixgarlic/potenday/global/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request ->
                         request.requestMatchers(
                                 new AntPathRequestMatcher("/oauth2/**"),
-                                new AntPathRequestMatcher("/test-without-auth")
+                                new AntPathRequestMatcher("/test-without-auth"),
                                 new AntPathRequestMatcher("/login/**"),
                                 new AntPathRequestMatcher("/")
                         ).permitAll()
