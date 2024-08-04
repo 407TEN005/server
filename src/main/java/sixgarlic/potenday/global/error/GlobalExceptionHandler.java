4be0cb73e029005366de60cc826a9cb2bccb4d5a
geon8692 <geon4735@gmail.com>
2024-08-05 00:52:03 +0900

    Feat: 예외처리 구현

---
+++ src/main/java/sixgarlic/potenday/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package sixgarlic.potenday.global.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleAccessDeniedException(AccessDeniedException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleNoSuchElementException(NoSuchElementException e) {
+        return e.getMessage();
+    }
+}
